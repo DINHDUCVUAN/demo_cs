f0603b55faf08f51646601a98df940f823728125
DINHDUCVUAN <dinhducvuan@gmail.com>
2024-09-22 15:52:27 +0700

    baitap

---
+++ trie9.py
@@ -0,0 +1,61 @@
+Trie = {  
+    "c": {  
+        "o": {  
+            "w": {  
+                "endWord": True  
+            },  
+            "endWord": False  
+        },  
+        "a": {  
+            "t": {  
+                "endWord": True  
+            },  
+            "r": {  
+                "endWord": True  
+            },  
+            "endWord": False  
+        },  
+        "endWord": False  
+        
+    },  
+    "b":{
+        "a": {
+            "n": {
+                "a": {
+                    "n": {
+                        "a": {
+                            "endWord": True
+                            }
+                        }
+                    }
+                }   
+        },
+    
+    "o": {  
+        "b": {  
+            "a": {  
+                "m": {  
+                    "a": {  
+                        "endWord": True  
+                        }  
+                    }  
+                }  
+            }  
+        }  
+    },
+    "a":{
+        "l": {
+            "i": {
+                "b": {
+                    "a": {
+                        "b": {
+                            "a": {
+                                "endWord": True  
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
